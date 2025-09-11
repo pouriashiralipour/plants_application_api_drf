ce0c8e70f78f92ce08ed021f0e3f09f6e6f85e6e
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-11 19:13:47 +0330

    override create_superuser

--- core/managers.py
+++ core/managers.py
@@ -25,5 +25,20 @@ class CustomManager(BaseUserManager):
             user.set_password(password)
 
         user.save(using=self._db)
-
         return user
+
+    def create_superuser(self, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        # A superuser must have a primary identifier to log into the admin panel
+        if not extra_fields.get("email") and not extra_fields.get("phone_number"):
+            raise ValueError(_("Superuser must have an email or phone number."))
+
+        return self.create_user(password, **extra_fields)
