085739ecc2e4bdceb329959424ad1eec81b16233
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 00:26:24 +0330

    create serializer for profile

--- core/serializers.py
+++ core/serializers.py
@@ -59,3 +59,72 @@ class OTPVerifySerializer(serializers.Serializer):
             raise serializers.ValidationError(_("Invalid or expired OTP."))
 
         return attrs
+
+
+class ProfileCompletionSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "gender",
+            "nickname",
+            "email",
+            "phone_number",
+            "password",
+        ]
+        extra_kwargs = {
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+            "date_of_birth": {"required": True},
+            "email": {"required": False},
+            "phone_number": {"required": False},
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "request" in self.context:
+            user = self.context["user"]
+
+            if user.email:
+                self.fields["email"].read_only = True
+                self.fields["phone_number"].required = True
+
+            elif user.phone_number:
+                self.fields["phone_number"].read_only = True
+                self.fields["email"].required = True
+
+    def validate(self, attrs):
+        user = self.context["request"].user
+        email = attrs.get("email", "").strip().lower()
+        phone = normalize_iran_phone(attrs.get("phone_number", ""))
+
+        if email and not user.email:
+            if User.objects.filter(email=email).exclude(pk=user.pk).exists():
+                raise serializers.ValidationError(
+                    {"email": "This email is already in use."}
+                )
+
+        if phone and not user.phone_number:
+            if User.objects.filter(phone_number=phone).exclude(pk=user.pk).exists():
+                raise serializers.ValidationError(
+                    {"phone_number": "This phone number is already in use."}
+                )
+        return attrs
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data.pop("password"))
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if "email" in validated_data and not instance.is_email_verified:
+            instance.is_email_verified = True
+        if "phone_number" in validated_data and not instance.is_phone_verified:
+            instance.is_phone_verified = True
+
+        instance.save()
+        return instance
