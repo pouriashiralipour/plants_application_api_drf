f582c0082c67d510b781142d2673b59e28e97b0e
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-11 19:20:33 +0330

    create full name methos

--- core/models.py
+++ core/models.py
@@ -67,3 +67,7 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email or self.phone_number or str(self.id)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
