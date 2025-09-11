9bbf65ea369a67deeea7779c1994cac6c723fc41
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-11 18:44:21 +0330

    override email field

--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(
+        max_length=254, unique=True, blank=True, null=True, verbose_name=_("email")
+    )
