9a6e328c6ac25eed4ee8e4a637e593347fe9f0d3
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 00:50:49 +0330

    create serializer for set new password

--- core/serializers.py
+++ core/serializers.py
@@ -204,3 +204,16 @@ class PasswordResetVerifySerializer(serializers.Serializer):
 
         request.session["reset_user_id"] = user.id
         return attrs
+
+
+class PasswordResetSetPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError(
+                {"password_confirm": _("Passwords do not match.")}
+            )
+
+        return attrs
