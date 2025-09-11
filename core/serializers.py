3f88628c6c062302ce8a2fed6a681d885ebff580
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 00:10:32 +0330

    create verify otp code serializer

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
+from .services import OTPService
 from .utils import normalize_iran_phone
 
 User = get_user_model()
@@ -36,3 +37,25 @@ class OTPRequestSerializer(serializers.Serializer):
             raise serializers.ValidationError(_("User with this identifier not found."))
 
         return value
+
+
+class OTPVerifySerializer(serializers.Serializer):
+    code = serializers.CharField(max_length=6, write_only=True)
+
+    def validate(self, attrs):
+        request = self.context["request"]
+        code = attrs["code"]
+
+        target = request.session.get("otp_target")
+        purpose = request.session.get("otp_purpose")
+
+        if not target or not purpose:
+            raise serializers.ValidationError(
+                _("No active OTP request found. Please request a new code.")
+            )
+
+        otp_data = OTPService.verify_otp(target=target, code=code, purpose=purpose)
+        if not otp_data:
+            raise serializers.ValidationError(_("Invalid or expired OTP."))
+
+        return attrs
