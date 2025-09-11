5b53eebc3ce56e6bf6b84cfeda891c8b92c67528
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-11 19:51:22 +0330

    add drf to installed apps

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party apps
+    "rest_framework",
+    "rest_framework_simplejwt",
     # my apss
     "core",
 ]
