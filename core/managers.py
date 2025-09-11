2e35ef89155a21c4124e2c7f1fbd6b030da59356
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-11 19:12:55 +0330

    create user method override for remove username in register

--- core/managers.py
+++ core/managers.py
@@ -1,5 +1,9 @@
+import uuid
+
 from django.contrib.auth.models import BaseUserManager
 from django.utils.translation import gettext_lazy as _
+
+from .utils import normalize_iran_phone
 
 
 class CustomManager(BaseUserManager):
@@ -9,5 +13,17 @@ class CustomManager(BaseUserManager):
 
         if email := extra_fields.get("email"):
             extra_fields["email"] = self.normalize_email(email)
-        # if phone_number := extra_fields.get("phone_number"):
-        #     extra_fields["phone_number"] = self.normalize_email(email)
+        if phone_number := extra_fields.get("phone_number"):
+            extra_fields["phone_number"] = self.normalize_iran_phone(phone_number)
+
+        if "username" not in extra_fields:
+            extra_fields["username"] = str(uuid.uuid4)
+
+        user = self.model(**extra_fields)
+
+        if password:
+            user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
