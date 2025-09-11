a51f8c1eb50dce2350bd9dd4d894f8cd7d1ba055
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 02:58:06 +0330

    add url

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("core.urls")),
+    path("", include("core.urls")),
 ]
 
 if settings.DEBUG:
--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import AuthViewSet, UserViewSet
 
 router = DefaultRouter()
+router.register("auth", AuthViewSet, basename="auth")
 router.register("users", UserViewSet, basename="users")
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
