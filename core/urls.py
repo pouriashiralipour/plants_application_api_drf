f15371434e59b6c19b3f6a5b01a9d897fcee0db3
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 00:26:12 +0330

    create  path for verify

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import OTPRequestView
+from .views import OTPRequestView, OTPVerifyView
 
 app_name = "core"
 
 urlpatterns = [
     # OTP-based flows (Registration and Login)
     path("otp/request/", OTPRequestView.as_view(), name="otp-request"),
+    path("otp/verify/", OTPVerifyView.as_view(), name="otp-verify"),
 ]
