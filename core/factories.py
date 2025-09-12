3b22b1736e4e0b37c09789882f058e563d1a4169
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-12 04:09:14 +0330

    fix factory code and update code for wargings

--- core/factories.py
+++ core/factories.py
@@ -5,16 +5,12 @@ User = get_user_model()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        skip_postgeneration_save = True
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    email = factory.Faker("email")
-    phone_number = factory.Sequence(lambda n: f"+98912{n:07d}")
-    is_email_verified = True
-    is_phone_verified = True
-
-    class Meta:
-        model = User
 
     @factory.post_generation
     def password(self, create, extracted, **kwargs):
