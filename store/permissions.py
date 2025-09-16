ca2b437c71936d261887ece4d6583b4aee81bbbd
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-16 14:11:08 +0330

    add permission for review

--- store/permissions.py
+++ store/permissions.py
@@ -6,3 +6,16 @@ class IsAdminOrReadOnly(BasePermission):
         return bool(
             request.method in SAFE_METHODS or (request.user and request.user.is_staff)
         )
+
+
+class ReviewPermission(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS
+            or (request.user and request.user.is_authenticated)
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in SAFE_METHODS or (request.user and request.user.is_staff)
+        )
